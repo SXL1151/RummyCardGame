a58c4f4ddd4e21c005f5ea65625dbc244b8e602d
SXL1151 <sxl1151@g.coppellisd.com>
2025-10-04 17:46:27 -0500

    Create main2.py

---
+++ main2.py
@@ -0,0 +1,23 @@
+import streamlit as st
+st.session_state.intro = True
+st.title("Rummy")
+st.markdown('''
+:red[Welcome to the rummy card game!]
+''')
+try:
+playerCt = st.selectbox("How many players will be playing?", ("2", "3", "4", "5",
+"6"))
+st.text(f"{playerCt} players will be playing")
+if playerCt == "2":
+cardsPerHand = 13
+elif playerCt == ("3") or playerCt == ("4"):
+cardsPerHand = 7
+else:
+cardsPerHand = 6
+st.success(f"Each player will recieve {cardsPerHand} cards")
+if playerCt:
+if butPress:
+except:
+pass
+butPress = st.button("Start Game")
+st.success("Game has started!")

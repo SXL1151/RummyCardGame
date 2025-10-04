7e0f1839454879611aac863c0a361ad5b5ebb1df
SXL1151 <sxl1151@g.coppellisd.com>
2025-10-04 17:50:44 -0500

    Update main.py

--- main.py
+++ main.py
@@ -5,19 +5,19 @@ st.markdown('''
 :red[Welcome to the rummy card game!]
 ''')
 try:
-playerCt = st.selectbox("How many players will be playing?", ("2", "3", "4", "5",
+  playerCt = st.selectbox("How many players will be playing?", ("2", "3", "4", "5",
 "6"))
-st.text(f"{playerCt} players will be playing")
-if playerCt == "2":
-cardsPerHand = 13
-elif playerCt == ("3") or playerCt == ("4"):
-cardsPerHand = 7
-else:
-cardsPerHand = 6
-st.success(f"Each player will recieve {cardsPerHand} cards")
-if playerCt:
-if butPress:
+  st.text(f"{playerCt} players will be playing")
+  if playerCt == "2":
+    cardsPerHand = 13
+  elif playerCt == ("3") or playerCt == ("4"):
+    cardsPerHand = 7
+  else:
+    cardsPerHand = 6
+    st.success(f"Each player will recieve {cardsPerHand} cards")
+    if playerCt:
+      butPress = st.button("Start Game")
+    if butPress:
+      st.success("Game has started!")
 except:
-pass
-butPress = st.button("Start Game")
-st.success("Game has started!
+    pass

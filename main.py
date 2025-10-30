bcab6b3a362425db1f77acacd85e213c278beb8b
SXL1151 <sxl1151@g.coppellisd.com>
2025-10-30 12:32:00 -0500

    Update main.py

--- main.py
+++ main.py
@@ -556,6 +556,7 @@ if st.session_state.handShuff == False:
                                     continue
                             if st.button("Pass to next person"): #Asks player to pass the device to the next player
                                 st.session_state.button = False
+                                st.session_state.handShuff = True
                                 st.session_state.clicks += 1
                                 continue
                         st.session_state.hand[0][0][:st.session_state.cardsPerHand] = new_hand
@@ -574,8 +575,7 @@ if st.session_state.handShuff == False:
                     if st.button("Knock", key=f"buttonknock {st.session_state.clicks}{i}{j}{z}"):
                         st.session_state.end = True
                         st.rerun()
-                    else:
-                        st.stop()
+                    st.stop()
 
 
                 

896ed577a54098f315dd929182db2b4fbfb86fe5
SXL1151 <sxl1151@g.coppellisd.com>
2025-10-30 13:01:29 -0500

    Update main.py

--- main.py
+++ main.py
@@ -571,10 +571,8 @@ if st.session_state.handShuff == False:
                         st.rerun()
                     if st.button("Call Gin", key=f"buttongin {st.session_state.clicks}{i}{j}{z}"):
                         st.session_state.end = True
-                        st.rerun()
                     if st.button("Knock", key=f"buttonknock {st.session_state.clicks}{i}{j}{z}"):
                         st.session_state.end = True
-                        st.rerun()
 
 
                 
